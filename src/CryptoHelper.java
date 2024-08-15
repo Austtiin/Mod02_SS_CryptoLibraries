49560a3e9944c3896ea7d6b8829d7c24a5c5e11e
Austtiin <austinstephens103@gmail.com>
2024-08-14 22:38:17 -0500

    setup

---
+++ src/CryptoHelper.java
@@ -0,0 +1,11 @@
+//CryptoHelper.java
+//This class will contain the logic for generating the MD5 hash of the user input
+
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+
+ class CryptoHelper {
+}
---
+++ src/Main.java
@@ -0,0 +1,32 @@
+//main.java
+//This will be the main entry of the prgoram
+
+//Austin Stephens
+//Professor Zayas
+//CEN4071C
+//Module 02 Assignment - Crypto Libraries
+
+//Instructions
+//Develop a Java program that:
+//imports one of the crypto libraries
+//takes in keyboard text input
+//generates and prints the MD5 hash of the keyboard text input
+
+//we will be using the widely used Bouncy Castle library for this assignment
+//Bouncy Castle is a collection of APIs used in cryptography. It is also the name of the company that developed it.
+
+
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+//we will have a seperate class for the main entry of the program
+//this will allow us to keep the main entry of the program seperate from the actual logic of the program
+//we will also use helper classes to keep the code clean and organized
+// our helper class will be called CryptoHelper
+//this class will contain the logic for generating the MD5 hash of the user input
+
+public class Main {
+
+}
